1f8d57db58c658f068ac11944e0d9c2dcb066491
惠成功chenggong <1902745538@qq.com>
2021-10-18 12:14:03 +0200

    add double

--- 1-MD_tutorial/1-1-GMX_installation/cmake_GMX2021.3_CPU_double.sh
+++ 1-MD_tutorial/1-1-GMX_installation/cmake_GMX2021.3_CPU_double.sh
@@ -7,5 +7,6 @@ cmake ../gromacs-2021.3 \
   -DREGRESSIONTEST_PATH=../regressiontests-2021.3 \
   -DCMAKE_C_COMPILER=icc \
   -DCMAKE_CXX_COMPILER=icpc \
+  -DGMX_DOUBLE=on \
   -DCMAKE_INSTALL_PREFIX=~/Software/GMX/2021.3-icc-double  
 #change the INSTALL_PREFIX according to your need
