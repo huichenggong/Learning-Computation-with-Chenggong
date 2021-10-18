dcd8a0182706c9853459d791c887d3eccaea5218
惠成功chenggong <1902745538@qq.com>
2021-10-18 11:08:31 +0200

    cmake sample for GMX2021.3. CPU. double precision.

---
+++ 1-MD_tutorial/1-1-GMX_installation/cmake_GMX2021.3_CPU_double.sh
@@ -0,0 +1,11 @@
+mkdir build-icc-double  
+cd build-icc-double  
+pwd
+cmake ../gromacs-2021.3 \
+  -DGMX_BUILD_OWN_FFTW=ON \
+  -DREGRESSIONTEST_DOWNLOAD=OFF \
+  -DREGRESSIONTEST_PATH=../regressiontests-2021.3 \
+  -DCMAKE_C_COMPILER=icc \
+  -DCMAKE_CXX_COMPILER=icpc \
+  -DCMAKE_INSTALL_PREFIX=~/Software/GMX/2021.3-icc-double  
+#change the INSTALL_PREFIX according to your need
