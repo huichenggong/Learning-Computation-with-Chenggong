81c2e3a1a5310bc43c1be9042207d4c731f40cac
Chenggong <chenggong.hui@mpinat.mpg.de>
2024-05-01 16:23:35 +0200

    random partition

--- CC_news_014_alchemical_pKa_calculation_and_error_cancellation/pKa_matrix/Charmm36m/JOB/03-HRE/02-win16/run_06_submit.sh
+++ CC_news_014_alchemical_pKa_calculation_and_error_cancellation/pKa_matrix/Charmm36m/JOB/03-HRE/02-win16/run_06_submit.sh
@@ -3,6 +3,8 @@
 cd ../../../
 base=$PWD
 
+partition=("p32" "p16" "p16" "p16" "p16")
+
 for i in $(ls -d ???-???); do
     cd $base/$i/03-HRE
     pwd
@@ -11,6 +13,9 @@ for i in $(ls -d ???-???); do
         cd $base/$i/03-HRE/run_$rep/02-win16/
         cp $base/tmp/03-HRE/run_X/02-win16/jobscript.MD_tmp.sh ./
         sed -i "s/JOB_NAME/$i-$rep/g" jobscript.MD_tmp.sh
+        pindex=$(($RANDOM % ${#partition[@]}))
+        part_i=${partition[$pindex]}
+        sed -i "s/PINDEX/$part_i/g" jobscript.MD_tmp.sh
         sbatch jobscript.MD_tmp.sh
     done
 done
